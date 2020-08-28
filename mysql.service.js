d97f474e92b43ba07caf8bcbb622b5f4bf92b9a1
franck-yvetot <44170458+franck-yvetot@users.noreply.github.com>
2020-08-28 23:30:11 +0200

    mysql as unified db interface

--- mysql.service.js
+++ mysql.service.js
@@ -14,7 +14,7 @@ async init(config) {
     return;
 
       this.config=config;
-  this.conPath = config.conPath || '.MySql.json';
+  this.conPath = config.conPath || '.MySql';
   }
 
   async connect() {
@@ -84,9 +84,201 @@ async query(q,values) {
     else
       res = await this.con.query(q);
 
+    if(res.insertId)
+      return res.insertId;
+
     const [rows, fields] = res;     
     return rows;
   }
+
+  /* ============ SUPPORT INTERFACE UNIFIEE BASEE SUR MONGODB ================= */
+  async _mapWhere(query) {
+    var where = "";
+    if(query)
+        objectSce.forEachSync(query, (value,name)=> {
+            where += " "+name + "='"+value+"'";
+        });
+    return where;
+}
+
+async _mapLimit(limit=0,skip=0) {
+    let s='';
+    if(limit) 
+    {
+        s = " LIMIT "+skip+" "+limit;
+    }
+
+    return s;
+}
+
+async findOne(query,col) {
+
+    const where = this._mapWhere(query);
+    col = col || config.table;
+    let qs = this.queries.findOne || "select * from "+col;
+    qs += where;
+
+    const docs = await this.db.query(qs);
+    if(docs.length>=1)
+        return docs[0];
+    
+    return null;
+}
+
+async find(query,col,limit=0,skip=0) {
+    const where = this._mapWhere(query);
+    col = col || config.table;
+    const qlimit = this._mapLimit(limit,skip);
+
+    let qs = this.queries.find || "select * from "+col;
+    qs += where;
+    qs += qlimit;
+
+    const docs = await this.db.query(qs);
+    return docs;
+}
+
+async count(query,col,limit=0,skip=0) {
+    const where = this._mapWhere(query);
+    col = col || config.table;
+    const qlimit = this._mapLimit(limit,skip);
+
+    let qs = this.queries.count || "count * from "+col;
+    qs += where;
+    qs += qlimit;
+
+    const docs = await this.db.query(qs);
+    return docs;
+}
+
+async insertOne(doc,col) {
+    col = col || config.table;
+
+    let qs = this.queries.insertOne || "INSERT INTO "+col;
+
+    let fnames = [];
+    let values = [];
+    objectSce.forEachSync(doc,(value,name)=> {
+        fnames.push(name);
+        values.push(value);
+    });
+
+    qs += "("+fnames.join(",")+") "+ "VALUES ('"+values.join("','")+"')";
+
+    const insertId= await this.db.query(qs);
+    
+    return insertId;
+}
+
+async insertMany(docs,col) {
+    col = col || config.table;
+    let qs = this.queries.insertMany || "INSERT INTO "+col;
+
+    if(docs.length == 0)
+        return null;
+   
+    const doc = docs[0];
+    let fnames = [];
+    objectSce.forEachSync(doc,(value,name)=> {
+        fnames.push(name);
+    });
+   
+    let aValues = [];
+    docs.forEach(row => {
+        let values = [];
+        objectSce.forEachSync(row,(value)=> {
+            values.push(value);
+        });
+        aValues.push("('"+values.join("','")+"')");
+    });
+
+    let qvals = aValues.join(",");
+    qs += " ("+fnames.join(",")+") "+ "VALUES ("+qvals+")";
+  
+    return await this.db.query(qs);
+}
+
+async updateOne(query,doc,col,addIfMissing=true) {
+
+    col = col || config.table;
+    let qs = this.queries.updateOne || 
+        (addIfMissing ? "REPLACE " : "UPDATE ")+col+" SET ";
+
+    const where = this._mapWhere(query);
+
+    let fnames = [];
+    let values = [];
+    objectSce.forEachSync(doc,(value,name)=> {
+        fnames.push(name);
+        values.push(value);
+    });
+
+    qs += "("+fnames.join(",")+") "+ "VALUES ('"+values.join("','")+"')";
+    qs += where;
+
+    const res = await this.db.query(qs);
+    
+    return res;
+}
+
+async updateMany(query,docs,col,addIfMissing=true) {
+    if(docs.length == 0)
+        return null;
+
+    const where = this._mapWhere(query);
+
+    col = col || config.table;
+    let qs = this.queries.updateMany || 
+        (addIfMissing ? "REPLACE " : "UPDATE ") +col+" SET ";
+   
+    const doc = docs[0];
+    let fnames = [];
+    objectSce.forEachSync(doc,(value,name)=> {
+        fnames.push(name);
+    });
+   
+    let aValues = [];
+    docs.forEach(row => {
+        let values = [];
+        objectSce.forEachSync(row,(value)=> {
+            values.push(value);
+        });
+        aValues.push("('"+values.join("','")+"')");
+    });
+
+    let qvals = aValues.join(",");
+    qs += " ("+fnames.join(",")+") "+ "VALUES ("+qvals+")";
+    qs += where;
+
+    const res = await this.db.query(qs);
+    
+    return res;
+}
+
+async deleteOne(query,col) {
+    col = col || config.table;
+    let qs = this.queries.deleteOne || "DELETE FROM "+col;
+        
+    const where = this._mapWhere(query);
+    qs += where;
+
+    const res = await this.db.query(qs);
+    
+    return res;
+}
+
+async deleteMany(query,col) {
+    col = col || config.table;
+    let qs = this.queries.deleteMany || "DELETE FROM "+col;
+        
+    const where = this._mapWhere(query);
+    qs += where;
+
+    const res = await this.db.query(qs);
+    
+    return res;
+}  
+
 }
 
 class MySqlSce
