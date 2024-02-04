52172d347cde15b47d4d2454406bb124ba97bdd5
supportPsoft <68959708+supportPsoft@users.noreply.github.com>
2024-02-04 11:04:00 +0100

    added typings

--- types/db_model.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@nxn_db_model' 
-{
-
-    import { SchemaField,SchemaFieldEnum,DbView,DbModelInstance,DbModel,DbModelSce} from "../db_model.service";
-    export { SchemaField,SchemaFieldEnum,DbView,DbModelInstance,DbModel,DbModelSce };
-
-}
